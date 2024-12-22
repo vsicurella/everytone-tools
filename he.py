add argparse, weighting options, fix basis direction

he.py
@@ -4,21 +4,21 @@
 
 """
 
-
-import utils
-import numpy as np
-
-import matplotlib.pyplot as plt
-# from scipy.signal import fftconvolve
-
+import argparse
 import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+from utils import *
 
 CENTS_ROUND=3
 RATIO_ROUND=6
 
 def farey_set_to_basis(ndSet, octaves=1):
     size = ndSet.shape[0]
-    period_shape = (size * octaves - 1, 2)
+    period_shape = (size * octaves - int(octaves > 1), 2)
 
     basis = np.zeros(period_shape, dtype=np.uint)
     basis[:size, 0] = ndSet[:, 0] + ndSet[:, 1]
@@ -38,7 +38,14 @@ def ratio_to_cents(ratio):
     return np.round(np.log2(ratio) * 1200, CENTS_ROUND)
 
 def show_plot(data, title="Plot", xlabel="X", ylabel="Y", **kwArgs):
-    plt.plot(data)
+    default_label = ylabel
+    if "label-default" in kwArgs:
+        default_label = kwArgs["label-default"]
+        del kwArgs["label-default"]
+
+    plt.plot(data, label=default_label)
+    num_plots=1
+
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -49,9 +56,22 @@ def show_plot(data, title="Plot", xlabel="X", ylabel="Y", **kwArgs):
                 txtpos = (0, max(data))
                 plt.annotate(kwArgs[kw], txtpos)
             elif kw == "xticks":
-                plt.xticks(kwArgs[kw])
+                labels=None
+                if "xtickLabels" in kwArgs:
+                    labels = kwArgs["xtickLabels"]
+                plt.xticks(kwArgs[kw], labels=labels)
             elif kw.startswith("plot"):
-                plt.plot(kwArgs[kw])
+                if kw.endswith("label"):
+                    continue
+                
+                labelKw = f'{kw}-label'
+                label = kwArgs[labelKw]
+                plt.plot(kwArgs[kw], label=label)
+                num_plots += 1
+
+    if num_plots > 1:
+        plt.legend()
+        # primary_plot.set_label(kwArgs['label-default'])
 
     plt.show()
 
@@ -69,7 +89,14 @@ class HarmonicEntropy:
     limit = None
     a = None
 
+    he3 = False
+
     basis_filter = None
+
+    weight_func = None
+    weight_func_name = None
+
+    kwArgs = None
 
     # state
     x_axis = None
@@ -80,25 +107,40 @@ class HarmonicEntropy:
     basis_ratios = None
     basis_cents = None
 
+    basis_triad_pedals = None
+    basis_triad_cents = None
+    basis_triad_x = None
+    basis_triad_y = None
+    basis_triad_xy = None
+
     x_length = None
     basis_length = None
 
-    Q = None
-    Qsum = None
-    P = None
     Entropy = None
+    EntropyAltWeights = None
 
     # pre-computes
     i_ss2 = None
     ssqrt2pi = None
 
-    def __init__(self, s=17, N=100, res=1, limit=2400, a=1):
-        self.update(s, N, res, limit, a)
+    tri_y_scalar = np.sqrt(3) / 2
+
+    def __init__(self, spread=17, N=1000, res=1, limit=2400, alpha=8, weight=None, he3=False, **kwArgs):
+        if weight is None:
+            weight = 'default'
+        self.he3 = he3
+        self.kwArgs = kwArgs
+        self.update(spread, N, res, limit, alpha)
+        self.prepareBasis()
+        self.setWeightingOption(weight)
 
     def suffix(self):
-        return "{}s_{}N_{}c_{}max".format(self.s, self.N, self.res, self.limit)
+        s = "{}s_{}a_{}N_{}c_{}max".format(self.s, self.a, self.N, self.res, self.limit)
+        if self.weight_func_name is not None:
+            s += "_" + self.weight_func_name
+        return s
 
-    def update(self, s=None, N=None, res=None, limit=None, a=None):
+    def update(self, s=None, N=None, res=None, limit=None, a=None, weight=None):
         if s is not None:
             self.s = s
         if N is not None:
@@ -109,14 +151,16 @@ class HarmonicEntropy:
             self.limit = limit
         if a is not None:
             self.a = a
+        if weight is not None:
+            self.setWeightingOption(weight)
 
-        self.x_axis = np.asarray(range(0, round(limit+res), res))
+        self.x_axis = np.arange(0, int(np.ceil(limit+res)), step=res)
         self.x_length = len(self.x_axis)
 
         self.num_periods = int(np.ceil(self.limit / 1200))
 
-        self.i_ss2 = 1 / (self.s**2 * 2);
-        self.ssqrt2pi = self.s * 2.50662827463;
+        self.i_ss2 = 1 / (self.s**2 * 2)
+        self.ssqrt2pi = self.s * 2.50662827463
 
     def prepareBasis(self, basis=None):
         if basis:
@@ -127,15 +171,13 @@ class HarmonicEntropy:
             self.basis_length = len(self.basis_cents)
             return
         
-        saveData=True
         file = os.path.join(os.path.dirname(__file__), "he_data", "farey{}.npy".format(self.N))
         if os.path.exists(file):
             basis = np.load(file)
         else:
             print("Calculating rationals...")
-            basis = utils.farey(self.N)
-            if saveData:
-                np.save(file, basis)
+            basis = farey(self.N)
+            np.save(file, basis)
 
         self.updateBasisSet(basis)
 
@@ -147,6 +189,17 @@ class HarmonicEntropy:
         self.basis_cents    = np.vectorize(ratio_to_cents)(self.basis_ratios)
         self.basis_length   = len(self.basis_cents)
 
+        if self.he3:
+            self.basis_triad_pedals = np.exp2(np.trunc(np.log2(self.basis_periods[:, 1])))
+            self.basis_periods = np.append(self.basis_periods, self.basis_triad_pedals.T[:, None], 1)
+            self.basis_triad_ratios = np.round(self.basis_periods[:, 1] / self.basis_periods[:, 2], RATIO_ROUND)
+            self.basis_triad_cents = np.vectorize(ratio_to_cents)(self.basis_triad_ratios)
+            mask = self.basis_triad_cents <= self.limit
+            filtered_cents = np.extract(mask, self.basis_triad_cents)
+            self.basis_triad_x = np.round(filtered_cents + (filtered_cents / 2)).astype(np.int64)
+            self.basis_triad_y = np.round(filtered_cents * self.tri_y_scalar).astype(np.int64)
+            self.basis_triad_xy = (np.extract(self.basis_triad_x <= self.limit, self.basis_triad_x), np.extract(self.basis_triad_y <= self.limit, self.basis_triad_y))
+
     def setBasisMask(self, mask):
         basis_n = np.extract(mask, self.basis_periods[:,0])
         basis_d = np.extract(mask, self.basis_periods[:,1])
@@ -155,49 +208,107 @@ class HarmonicEntropy:
         self.basis_cents    = np.extract(mask, self.basis_cents)
         self.basis_length   = len(self.basis_cents)
 
+        if self.he3:
+            self.basis_periods[:,2] = np.extract(mask, self.basis_periods[:,2])
+            self.basis_triad_pedals = np.extract(mask, self.basis_triad_pedals)
+            self.basis_triad_ratios = np.extract(mask, self.basis_triad_ratios)
+            self.basis_triad_cents = np.extract(mask, self.basis_triad_cents)
+            self.basis_triad_x = np.extract(mask, self.basis_triad_x)
+            self.basis_triad_y = np.extract(mask, self.basis_triad_y)
+            self.basis_triad_xy = (self.basis_triad_x, self.basis_triad_y)
+
     def setOddBasis(self):
         mask = self.basis_periods[:, 0] & self.basis_periods[:, 1] & 1
         self.setBasisMask(mask)
-        
-    def prepareWeights(self, weights=None):
-        if weights:
-            return
-        
-        print("Calculating weights...")
 
-        products = self.basis_periods[:,0] * self.basis_periods[:,1]
-        weights = np.reciprocal(self.ssqrt2pi * np.sqrt(products))
-        
-        diffs = np.asarray([ self.basis_cents[j] - self.x_axis for j in range(self.basis_length)])
-        self.Q = np.asarray([ np.exp(-np.square(diffs[j,:]) * self.i_ss2) * weights[j] for j in range(self.basis_length) ])
-        self.Qsum = self.Q.sum(axis=0)
+    def setWeightingFunction(self, func, name=None):
+        self.weight_func = func
+        if func is None:
+            self.weight_func_name = None
+        else:
+            self.weight_func_name = "wx" if name is None else name
 
-    def prepareProbabilities(self, probabilities=None):
-        if probabilities:
-            return
-        
-        print("Calculating probabilities...")
+    def setWeightingOption(self, option):
+        if option == 'default' or option is None or option == 'sqrtnd':
+            self.setDefaultWeightingFunction()
+        elif option == 'lencf':
+            self.setLenCfWeight()
+        elif option == 'lenmaxcf':
+            self.setLenMaxCfWeight()
+        elif option == 'sumcf':
+            self.setSumCfWeight()
+        elif option == 'all':
+            self.weight_func_name = option
+            self.EntropyAltWeights = {}
 
-        sigma = 1e-12
+            self.setDefaultWeightingFunction()
+            self.Entropy = self.convolveHRE()
+            self.setLenCfWeight()
+            self.EntropyAltWeights['lencf'] = self.convolveHRE()
+            self.setLenMaxCfWeight()
+            self.EntropyAltWeights['lenmaxcf'] = self.convolveHRE()
+            self.setSumCfWeight()
+            self.EntropyAltWeights['sumcf'] = self.convolveHRE()
 
-        self.P = np.zeros((self.basis_length, self.x_length))
+            self.setDefaultWeightingFunction()
 
-        for c in range(self.x_length):
-            self.P[:, c] = self.Q[:, c] / self.Qsum[c] + sigma
-        
-    def prepareEntropy(self, entropy=None):
-        if entropy:
-            return
-        
-        print("Calculating entropy...")
+        else:
+            raise Exception("Unknown weighing option: " + str(option))
+
+    def setDefaultWeightingFunction(self):
+        self.setWeightingFunction(None)
     
-        self.Entropy = np.zeros(self.x_length)
-        for c in range(self.x_length):
-            self.Entropy[c] = -sum(self.P[:,c] * np.log(self.P[:,c]))
+    def setLenCfWeight(self):
+        weigh_nd = lambda ns,ds: [ len(get_cf(ns[i]/ds[i])) for i in range(len(ns)) ]
+        def weigh(array):
+            shape = (self.basis_periods.shape[0], self.basis_periods.shape[1]-1)
+            weights = np.zeros(shape)
+            for c in range(shape[1]):
+                weights[:,c] = weigh_nd(array[:,c], array[:,c+1])
+            return weights
+        self.setWeightingFunction(weigh, "lencf")
+
+    def setLenMaxCfWeight(self):
+        prod = lambda cf: len(cf) * max(cf)
+        prod_nd = lambda ns,ds: [ prod(get_cf(ns[i]/ds[i])) for i in range(len(ns)) ]
+        def weigh(array):
+            shape = (self.basis_periods.shape[0], self.basis_periods.shape[1]-1)
+            weights = np.zeros(shape)
+            for c in range(shape[1]):
+                weights[:,c] = np.sqrt(prod_nd(array[:,c], array[:,c+1]))
+            return weights
+        self.setWeightingFunction(weigh, "sqrt(len(cf)*max(cf))")
+
+    def setSumCfWeight(self):
+        sum_nd = lambda ns,ds: [ sum(get_cf(ns[i]/ds[i])) for i in range(len(ns)) ]
+        def weigh(array):
+            shape = (self.basis_periods.shape[0], self.basis_periods.shape[1]-1)
+            weights = np.zeros(shape)
+            for c in range(shape[1]):
+                weights[:,c] = np.asarray(sum_nd(array[:,c], array[:,c+1]))
+            return weights
+        self.setWeightingFunction(weigh, "sum(cf)")
 
     def convolveHRE(self):
-        base_weighted = 1 / np.sqrt(self.basis_periods[:,0] * self.basis_periods[:,1])
-        base_weighted_alpha = np.power(base_weighted, self.a)
+        if self.weight_func is None:
+            base_weights = 1 / np.sqrt(np.prod(self.basis_periods, axis=1))
+        else:
+            base_weights = 1 / self.weight_func(self.basis_periods)
+
+        base_weighted_alpha = np.power(base_weights, self.a)
+
+        if self.he3:
+            K = np.zeros(shape=(self.x_length, self.x_length))
+            Ka = np.zeros(shape=(self.x_length, self.x_length))
+            
+            # splat dirac deltas into buffer
+            np.add.at(K, self.basis_triad_xy, base_weights)
+            np.add.at(Ka, self.basis_triad_xy, base_weights**alpha)
+            plt.figure(figsize=(6,6))
+            plt.imshow(np.log(1e-6+K), aspect="equal", origin='lower')
+            plt.axis('off')
+            plt.show()
+            return
 
         # turn basis cents into a sum of delta functions
         basis_index = np.rint(self.basis_cents / self.res).astype(np.uint32)
@@ -205,67 +316,128 @@ class HarmonicEntropy:
         K = np.zeros(self.x_length)
         Ka =  np.zeros(self.x_length)
         for ji in range(self.basis_length):
-            di = self.x_length - basis_index[ji] - 1
-            K[di] += base_weighted[ji]
+            di = basis_index[ji]
+            if di < 0 or di >= self.x_length:
+                print(f"Warning: ignoring basis overflow with {self.basis_periods[ji,0]}/{self.basis_periods[ji,1]}")
+                continue
+            K[di] += base_weights[ji]
             Ka[di] += base_weighted_alpha[ji]
 
-        # show_plot(K, "K")
+        # S = np.exp(-np.square(self.x_axis - self.x_length//2) * self.i_ss2) / self.ssqrt2pi
+        # s_range = round(self.s * 5)
+        # xr=np.arange(-self.ssqrt2pi, self.ssqrt2pi, self.res)
+        S = np.exp(-np.arange(-self.ssqrt2pi, self.ssqrt2pi, self.res)**2 * self.i_ss2)
+        # plt.plot(xr,S)
+        # plt.show()
         # return
+        sigma = 1e-16
+        alpha = self.a
+        if alpha == 1:
+            alpha = sigma
 
-        S = np.exp(-np.square(self.x_axis - self.x_length//2) * self.i_ss2) / self.ssqrt2pi
-        # show_plot(S, "S")
-        # return
-    
-        psi = np.convolve(K, S, 'same')
+        psi = signal.convolve(K, S, 'same')
+        pa = signal.convolve(Ka, S ** alpha, 'same')
 
-        if self.a == 1:
-            self.Entropy = psi / np.log(psi**self.a)
-            return True
+        return np.log(pa / (psi ** alpha) + sigma) / (1 - alpha)
 
-        pa = np.convolve(Ka, np.power(S, self.a), 'same')
-
-        self.Entropy = np.log(pa / psi ** self.a) / (1 - self.a)
-
-        return True
-
-    def calculate(self):
-        self.prepareBasis()
-        if self.convolveHRE():
+    def calculate(self, loadFile=True):
+        file = os.path.join(os.path.dirname(__file__), "he_data", "he_{}.npy".format(self.suffix()))
+        if loadFile and os.path.exists(file):
+            self.Entropy = np.load(file)
             return
-        # self.prepareWeights()
-        # self.prepareProbabilities()
-        # self.prepareEntropy()
+
+        self.Entropy = self.convolveHRE()
+
+    def writeEntropy(self):
+        file = os.path.join(os.path.dirname(__file__), "he_data", "he_{}".format(self.suffix()))
+        print(f"Writing: {file}")
+        np.save(file, self.Entropy)
+        np.savetxt(file+".txt", self.Entropy,  fmt="%f")
 
     def plot(self):
-        diff = np.diff(self.Entropy)
-        # diff2 = np.diff(diff)
-        ticks = []
-        for i in range(1, self.x_length - 1):
-            dx2 = np.square(diff[i])
-            if dx2 <= 1e-8:
-                ticks.append(self.x_axis[i])
-            # if abs(dx) < absMin:
-            #     absMin = abs(dx)
+        plot_data = self.Entropy
 
-        show_plot(self.Entropy, 
-                  "Harmonic Entropy sqrt(nd) weighting", 
+        plotArgs = {}
+
+        ticksKw = 'ticks'
+        if ticksKw in self.kwArgs and self.kwArgs[ticksKw]:
+            minima_index = signal.argrelextrema(self.Entropy, np.less)[0]
+            minima_entropy = self.Entropy[minima_index]
+
+            bins = int(5 / self.res)
+            bins += (1 - bins % 2)
+            hist, edges = np.histogram(minima_entropy, bins)
+
+            tick_edge = int(np.ceil(bins/2))
+            max_entropy = edges[tick_edge]
+            minima_ticks = []
+            for m in minima_index:
+                if self.Entropy[m] > max_entropy:
+                    continue
+                minima_ticks.append(m)
+            
+            ticks = [0, *minima_ticks, self.x_length - 1]
+            tickLabels = [ f"{(t*self.res):.2f}" for t in ticks ]
+            plotArgs["xticks"] = ticks
+            plotArgs["xtickLabels"] = tickLabels
+
+        weight_name = "sqrt(nd)" 
+        if self.weight_func_name is not None:
+            weight_name = self.weight_func_name
+
+        plotArgs['label-default'] = weight_name
+
+        if self.EntropyAltWeights is not None:
+            for kw in self.EntropyAltWeights:
+                key = f'plot-{kw}'
+                plotArgs[key] = self.EntropyAltWeights[kw]
+                plotArgs[f'{key}-label'] = kw
+            
+            weight_name = "all"
+
+        title = "Harmonic Entropy {} weighting".format(weight_name)
+
+        annotation = f"s={self.s}, N<{self.N}, a={self.a}"
+        if self.res != 1:
+            annotation += f", res={self.res}c"
+
+        plotArgs["annotate"] = annotation
+
+        show_plot(plot_data, 
+                  title, 
                   "Dyad (cents)", 
-                  "Dissonance", 
-                #   annotate="s={}, N<{}, a={}, {}c<{}".format(self.s, self.N, self.a, self.res, self.limit),
-                  annotate="s={}, N<{}, a={}".format(self.s, self.N, self.a),
-                #   xticks=ticks,
-                #   plotDiffs=diff,
-                #   plotDiffs2=diff2
+                  "Dissonance",
+                  **plotArgs
                   )
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser("harmonicentro.py")
+    parser.add_argument('-n', '--N', type=int, help="Exclusive limit of basis set ratio denominators", default=1000)
+    parser.add_argument('-s', '--spread',type=float, help="Bandwidth of spreading function in cents")
+    parser.add_argument('-a', '--alpha', type=float, help="Order of weight scaling", default=3)
+    parser.add_argument('-r', '--res', type=float, help="Resolution of x-axis in cents")
+    parser.add_argument('-l', '--limit', type=float, help="Last cents value to calculate")
+    parser.add_argument('-w', '--weight', choices=['default', 'sqrtnd', 'lencf', 'lenmaxcf', 'sumcf', 'all'])
+    parser.add_argument('--he3', action='store_true', help='3HE mode')
+    parser.add_argument('--plot', action='store_true', help="Display plot")
+    parser.add_argument('--ticks', action='store_true', help="Auto-select minima-based x-axis ticks")
+    parser.add_argument('--save', action='store_true', help="Save to file")
 
-    eightoctaves=ratio_to_cents(2**2)
+    parsed = parser.parse_args()
 
-    he = HarmonicEntropy(s=17, N=1000, a=3)
-    # he.calculate()
-    he.prepareBasis()
-    # he.setOddBasis()
-    he.convolveHRE()
-    he.plot()
+    options = vars(parsed)
+    save = options['save']
+    del options['save']
+    plot = options['plot']
+    del options['plot']
+
+    heArgs = { k:v for k,v in options.items() if v is not None }
+    he = HarmonicEntropy(**heArgs)
+    he.calculate()
+
+    if save:
+        he.writeEntropy()
+
+    if plot:
+        he.plot()