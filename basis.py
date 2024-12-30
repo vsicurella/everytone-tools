71cb880b45cbe0eb3addf2e77c7090109143e682
Vincenzo Sicurella <sicurella12@gmail.com>
2024-12-30 02:47:52 -0500

    start on Basis class

--- basis.py
+++ basis.py
@@ -4,74 +4,20 @@
 
 """
 
-import ctypes
-import os
 import numpy as np
 from math import gcd
-import numpy as np
-import utils
 from primes import *
+from farey import * 
+# from utils import *
 
+_RATIO_DB_ = None
+_USE_RATIO_DB_ = True
 try:
-    _c_farey_path=os.path.abspath('./c_farey/farey.so')
-    __utils = ctypes.CDLL(_c_farey_path)
-
-    __intp = ctypes.POINTER(ctypes.c_int)
-
-    _farey_len = __utils.farey_len
-    _farey_len.argtypes = [ctypes.c_int]
-    _farey_len.argtypes = [ctypes.c_ulonglong]
-
-    _farey_c = __utils.farey
-    _farey_c.argtypes = [ctypes.c_int, ctypes.c_ulonglong, __intp]
-
-    def _farey(n):
-        size = farey_len(n)
-        seq = np.zeros((size * 2), dtype=np.int32)
-        pseq = seq.ctypes.data_as(__intp)
-        _farey_c(n, size, pseq)
-        return seq.reshape((size, 2))
-
+    _RATIO_DB_ = __import__("ratio-db")
 except:
-    print("Error, unable to load utils binary, falling back to numpy version")
+    _USE_RATIO_DB_ = False
+    print("Unable to import ratio-db.py, perhaps due to a missing dependency. Using fallback code.")
 
-    def _farey_len(n):
-        len = n * (n + 3) // 2
-        p = 2
-        q = 0
-        while p <= n:
-            q = n // (n // p) + 1
-            len -= _farey_len(n // p) * (q - p)
-            p = q
-        return len
-    
-    def _farey(n):
-        f1 = (1, 0)
-        f2 = (n, 1)
-        
-        l = _farey_len(n)
-        nd_interleaved = np.zeros((l, 2), dtype=np.int64)
-        nd_interleaved[0, :] = f1[::-1]
-        nd_interleaved[1, :] = f2[::-1]
-
-        i = 2
-        while (f2[0] > 1):
-            k = (n + f1[0]) // f2[0]
-
-            t = f1
-            f1 = f2
-            f2 = (f2[0] * k - t[0], f2[1] * k - t[1])
-
-            nd_interleaved[i, :] = f2[::-1]
-            i+=1
-
-        return nd_interleaved
-
-def farey_len(n):
-    return _farey_len(n)
-
-def farey(n):
-    return _farey(n)
 
 # ndSet, basis: [ denominator, numerator ]
 def farey_set_to_basis(ndSet, harmonic_limit=2):
@@ -103,24 +49,25 @@ def farey_set_to_basis(ndSet, harmonic_limit=2):
 def get_farey_sequence_basis(N, harmonic_limit=2):
     return farey_set_to_basis(farey(N), harmonic_limit)
 
+def get_triplet_basis(N, harmonic_limit=2, product_limit=27_000_000, start_harmonic=1):
+    if harmonic_limit < 2:
+        raise Exception("harmonic_limit cannot be less than 2")
+    harmonic = int(harmonic_limit)
 
-def get_triplet_basis(N, harmonic_limit=2, product_limit=27_000_000):
-    harmonic = int(max(harmonic_limit, 2))
+    top_limit = N * harmonic
+    if start_harmonic < 1 or start_harmonic >= top_limit:
+        raise Exception(f"start_harmonic should be 1 or below {top_limit}")
+    start = int(start_harmonic)
 
     # generate all triples a:b:c within the period
     triplets = []
-    for i in range(1, N):
+    for i in range(start, N):
         for j in range(i, harmonic*i+1):
             for k in range(j, harmonic*i+1):
                 if i*j*k < product_limit and gcd(i,j,k) == 1:
                     triplets.append([i,j,k])
 
     return np.asarray(triplets)
-
-def nd_basis_to_cents(nd_periods):
-    to_cents = np.vectorize(utils.ratio_to_cents)
-    cents = to_cents(nd_periods[:,1:] / nd_periods[:,:-1])
-    return cents.squeeze()
 
 def create_mask_from_lambdas(basis_set, *funcs):
     def composite(*args):
@@ -178,11 +125,143 @@ def create_is_prime_test():
         return np.apply_along_axis(lambda row: np.all(np.vectorize(is_prime)(row)), 1, basis_set)
     return test
 
-if __name__ == '__main__':
-    N = 10
-    harmonic = 16
-    basis_set = get_farey_sequence_basis(N, harmonic)
+class BasisParams:
+    def __init__(self, complexity_limit, integer_limit=None, he3=False, noDyads=False):
+         self.complexity_limit = complexity_limit
+         self.integer_limit = integer_limit
+         self.he3 = he3
+         self.noDyads = noDyads
+class PrimeLimitBasisParams(BasisParams):
+    def __init__(self, limit, harmonic_limit, complexity_limit, integer_limit=None, he3=False, noDyads=False):
+        super().__init__(complexity_limit, integer_limit, he3, noDyads)
+        self.limit = limit
+        self.harmonic_limit = harmonic_limit
 
+class BasisInterface:
+    ratioDb = None
+
+    __dummy_find__=lambda tagsString,matchString,sortString,offset,queryLimit: -1
+    find_harmonics = __dummy_find__
+    find_dyads = __dummy_find__
+    find_triads = __dummy_find__
+    
+    get_farey_sequence_basis = lambda self, N, harmonic_limit: get_farey_sequence_basis(N, harmonic_limit)
+    get_prime_basis = lambda params: [] # TODO
+
+    _create_prime_limit_iterator_ = lambda prime, limit: iter(range(0)) # TODO
+
+    def _get_farey_sequence_basis_db_(N, harmonic_limit):
+        basis_set = BasisInterface.find_dyads('values', f'1200 >= any(cents) and prime_limit <= ({N*2-1}) and (numerator<={N} or denominator<={N}))', 'integer_limit DESC')
+        # todo figure this query out
+        return farey_set_to_basis(basis_set, harmonic_limit)
+    
+    def _get_harmonic_segment_db_(self, start=8, end=16, prime_limit=13, sortString=None, limit=0):
+        harmonics = self.find_harmonics(tagString="harmonic", matchString=f'prime_limit={prime_limit} and harmonic >= {start} and harmonic <= {end}', sortString=sortString, limit=limit)
+        return iter([ h[0] for h in harmonics ])
+    
+    def _create_harmonic_segment_iterator_db_(self, start=8, end=16, prime_limit=13):
+        harmonics = self._get_harmonic_segment_db_(start, end, prime_limit)
+        return iter(harmonics)
+
+    def _get_prime_basis_db_(self, params:PrimeLimitBasisParams):
+            prime_match = f'prime_limit<={params.limit}'
+            if params.complexity_limit:
+                prime_match += f' and complexity_limit<={params.complexity_limit}'
+            if params.integer_limit:
+                prime_match += f' and integer_limit<={params.integer_limit}'
+            prime_match += f' and (SELECT MAX(d) FROM unnest(decimals) AS d) <= {params.harmonic_limit}'
+            if params.he3:
+                triad_match = prime_match
+                if params.noDyads:
+                    triad_match += f' and (root < mediant and mediant < dominant)'
+                # triad_match += f' and '
+                return self.find_triads(matchString=triad_match, sortString='integer_limit ASC, root ASC')
+            return self.find_dyads(matchString=prime_match, sortString='any(cents) ASC')
+
+    def __init__(self):
+        if _USE_RATIO_DB_:
+            BasisInterface.ratioDb          = _RATIO_DB_.RatioDb()
+            self.find_harmonics             = BasisInterface.ratioDb.find_harmonics
+            self.find_dyads                 = BasisInterface.ratioDb.find_dyads
+            self.find_triads                = BasisInterface.ratioDb.find_triads
+            self.get_farey_sequence_basis   = self._get_farey_sequence_basis_db_
+            self.get_prime_basis            = self._get_prime_basis_db_
+            self._create_harmonic_segment_iterator_db_ = self._create_harmonic_segment_iterator_db_
+
+class Basis(BasisInterface):
+
+    def __init__(self,**kwArgs):
+        super().__init__()
+
+        self.he3 = False if "he3" not in kwArgs else kwArgs["he3"]
+        self.noDyads = False if "noDyads" not in kwArgs else kwArgs["noDyads"]
+
+        self.params = None if "params" not in kwArgs else kwArgs["params"]
+
+    def setToPrimeLimit(self, limit, harmonic_limit=2, complexity_limit=None, integer_limit=None):
+        # if complexity_limit:
+        #     self.prime_query.complexity_limit = complexity_limit
+        # else:
+            # end_harm = limit * harmonic_limit
+            # [[adj_harm]] = self.find_harmonics("harmonic", f"prime_limit={params.limit} and harmonic < {end_harm}", 'harmonic DESC', queryLimit=1)
+            # c_limit = (end_harm) ** (3 - (1 if params.he3 else 2)) * adj_harm
+            # c_limit = end_harm * harmonic_limit * (harmonic_limit if self.he3 else 1)
+            # c_limit = end_harm * harmonic_limit
+
+        if integer_limit is None:
+            integer_limit = limit * harmonic_limit ** 2
+
+        self.prime_query = PrimeLimitBasisParams(limit, harmonic_limit, complexity_limit, integer_limit, self.he3, self.noDyads)
+        if self.prime_query == self.params:
+            return
+
+        basis = self.get_prime_basis(self.prime_query)
+        self._set_basis_(basis) # move to interface?
+        self.params = self.prime_query
+
+    # def setToPrimodalStructure(self, prime, start=2, end=4):
+    #     self.prime_query = PrimeLimitBasisParams(prime, end, None, prime*end, self.he3, self.noDyads)
+    #     self.basis = self.get_prime_basis(self.prime_query)
+    #     self.params = self.prime_query
+
+    def setToIntegerLimit(self, int_limit, harmonic_limit):
+        #todo params
+        if self.he3:
+            self.basis = self.find_triads()
+        else:
+            return get_farey_sequence_basis(int_limit, harmonic_limit)
+        
+    def _set_basis_(self, basis):
+        self.basis = basis
+        self.values     = np.asarray([ data[1] for data in basis ])
+        self.labels     = np.asarray([ data[2] for data in basis ])
+        self.decimals   = np.asarray([ data[5] for data in basis ])
+        self.cents      = np.asarray([ data[7] for data in basis ])
+        self.he_weights = np.asarray([ data[17] for data in basis  ])
+        
+    def getSet(self):
+        return self.basis
+        
+    def getValues(self):
+        return self.values
+    
+    def getLabels(self):
+        return self.labels
+    
+    def getDecimals(self):
+        return self.decimals
+    
+    def getCents(self):
+        return self.cents
+    
+    def getWeights(self):
+        return self.he_weights
+
+if __name__ == '__main__':
+    
+    N = 5
+    harmonic = 4
+    basis_set = get_farey_sequence_basis(N, harmonic)
     duplicateCheck = {}
     i = 0
     for dyad in basis_set:
@@ -193,11 +272,14 @@ if __name__ == '__main__':
         duplicateCheck[dyad] = True
     print("successful dyad test")
 
-    print(f"N: {N}, set size: {len(basis_set)}")
-    basis_cents = nd_basis_to_cents(basis_set)
+    # print(f"N: {N}, set size: {len(basis_set)}")
+    # basis_cents = nd_basis_to_cents(basis_set)
 
-    triplet_set = get_triplet_basis(N, harmonic)
-    print(f'\ttriplet size: {len(triplet_set)}')
-    triplet_cennts = nd_basis_to_cents(triplet_set)
+    # triplet_set = get_triplet_basis(N, harmonic)
+    # print(f'\ttriplet size: {len(triplet_set)}')
+    # triplet_cennts = nd_basis_to_cents(triplet_set)
+
+    basis = Basis(he3=True)
+    basis.setToPrimeLimit(5, 4)
 
     print("Done")
